c5740010340c7096d2ddbced0b8f4c908e188164
Satish Shaukari <satya1806@gmail.com>
2017-09-11 23:04:39 +0530

    Refactor

--- app/src/main/java/lm/com/lmapp/CustomPagerAdapter.java
+++ app/src/main/java/lm/com/lmapp/CustomPagerAdapter.java
@@ -1,12 +1,15 @@
 package lm.com.lmapp;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by skumar128 on 9/11/17.
@@ -16,8 +19,10 @@ public class CustomPagerAdapter extends PagerAdapter implements Constants{
 
     private Context mContext;
     private String mCurr;
-    int [] arrImage = new int[] {
-        R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher
+    String [] arrImage = new String[] {
+            "http://be962883fa4932cb8c45-4918b6c23895973d0d77439479dabaa9.r81.cf3.rackcdn.com/119580217-PA008_01-800.jpg",
+            "http://be962883fa4932cb8c45-4918b6c23895973d0d77439479dabaa9.r81.cf3.rackcdn.com/117514970-MOD9611_01-800.jpg",
+            "http://7363c8e5d644af3f61fe-f801ca07733addcf236da446f6ef5b12.r6.cf3.rackcdn.com/BA68420_01-215.jpg"
     };
     String [] arrTitle = new String[]{
             "Title 1","Title 2","Title 3"
@@ -42,8 +47,8 @@ public class CustomPagerAdapter extends PagerAdapter implements Constants{
         ImageView image = (ImageView) layout.findViewById(R.id.image);
         TextView title = (TextView) layout.findViewById(R.id.title);
         TextView price = (TextView) layout.findViewById(R.id.price);
-        image.setImageResource(arrImage[position]);
-
+        //image.setImageURI(Uri.parse("http://be962883fa4932cb8c45-4918b6c23895973d0d77439479dabaa9.r81.cf3.rackcdn.com/119580217-PA008_01-800.jpg"));
+        Picasso.with(mContext).load(arrImage[position]).into(image);
         switch (mCurr){
             case INR:
                 prefix = "\u20B9";
@@ -82,4 +87,6 @@ public class CustomPagerAdapter extends PagerAdapter implements Constants{
     public float getPageWidth(int position) {
         return 0.5f;
     }
+
+
 }
